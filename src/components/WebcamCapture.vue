6ba275b95fb74ad4caff894fd220cbaaf7675db1
choigawoon <choigawoon@gmail.com>
2024-03-25 00:12:04 +0900

    WebcamCapture.vue 파일에서 비디오 해상도를 가져오고 비디오 크기를 조정하는 기능을 추가했습니다.

--- src/components/WebcamCapture.vue
+++ src/components/WebcamCapture.vue
@@ -3,7 +3,7 @@ import { ref, onMounted } from 'vue'
 
 const myNumber = ref<string[]>([''])
 const duration = ref<number>(0)
-const video = ref<HTMLElement>()
+const video = ref<HTMLVideoElement>()
 const canvas = ref<HTMLElement>()
 const preview = ref<string>('')
 const capture = async () => {
@@ -29,7 +29,7 @@ const capture = async () => {
     }
     formData.append('file', blob);
 
-    const response = await fetch('http://localhost:8000/upload/', {
+    const response = await fetch('/api/upload/', {
       method: 'POST',
       body: formData
     });
@@ -69,7 +69,7 @@ const inputText = "[2024/03/24 17:23:40] ppocr INFO: [[[103.0, 144.0], [132.0, 1
 const results = extractOcrResults(inputText);
 console.log(results);
 myNumber.value = results;
-
+const resolution = ref('')
 onMounted(() => {
   navigator.mediaDevices.getUserMedia({
     video: {
@@ -89,14 +89,63 @@ onMounted(() => {
     .then(stream => {
       if (video.value instanceof HTMLVideoElement) {
         video.value.srcObject = stream;
+        // 스트림으로부터 실제 비디오 해상도를 가져옵니다.
+        const track = stream.getVideoTracks()[0];
+        const settings = track.getSettings();
+        if (!settings) {
+          return;
+        }
+        const actualWidth = settings.width;
+        const actualHeight = settings.height;
+
+        resolution.value = `${actualWidth}x${actualHeight}`;
+        console.log(`Actual video resolution: ${actualWidth}x${actualHeight}`)
+
+        // <video> 요소의 크기를 실제 비디오 해상도에 맞춰 조정합니다.
+        if (!video.value) {
+          return;
+        }
+
+        // check if mobile
+        const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+        if (!isMobile) {
+          return;
+        }
+
+        if (actualWidth) {
+          video.value.width = actualWidth;
+        }
+        if (actualHeight) {
+          video.value.height = actualHeight;
+        }
       }
     });
 })
+const playVideo = () => {
+  const _video = video.value as HTMLVideoElement;
+  if (_video) {
+    _video.play().catch(e => console.error('Error playing the video:', e));
+  }
+}
+
+const pauseVideo = () => {
+  const _video = video.value as HTMLVideoElement;
+  if (_video) {
+    _video.pause();
+  }
+}
 </script>
 
 <template>
   <div class="flex flex-col bg-slate-100 rounded justify-center items-center">
+    <p>{{ resolution }}</p>
     <video class="mt-4 rounded-xl" ref="video" width="320" height="240" autoplay></video>
+    <div class="flex space-x-2 mt-4">
+      <button class="mx-auto text-white rounded bg-blue-500 hover:bg-blue-600 p-2 text-lg font-bold font-serif"
+        @click="playVideo">Play</button>
+      <button class="mx-auto text-white rounded bg-red-500 hover:bg-red-600 p-2 text-lg font-bold font-serif"
+        @click="pauseVideo">Pause</button>
+    </div>
     <button class="mt-4 mx-auto text-white rounded bg-blue-500 hover:bg-blue-600 p-4 text-2xl font-bold font-serif"
       @click="capture">Capture</button>
     <canvas ref="canvas" width="320" height="240" style="display: none;"></canvas>
@@ -112,10 +161,3 @@ onMounted(() => {
     </div>
   </div>
 </template>
-
-<!--
-[2024/03/24 16:46:16] ppocr DEBUG: Namespace(help='==SUPPRESS==', use_gpu=False, use_xpu=False, use_npu=False, ir_optim=True, use_tensorrt=False, min_subgraph_size=15, precision='fp32', gpu_mem=500, gpu_id=0, image_dir='temp.jpg', page_num=0, det_algorithm='DB', det_model_dir='/home/choigawoon/.paddleocr/whl/det/ml/Multilingual_PP-OCRv3_det_infer', det_limit_side_len=960, det_limit_type='max', det_box_type='quad', det_db_thresh=0.3, det_db_box_thresh=0.6, det_db_unclip_ratio=1.5, max_batch_size=10, use_dilation=False, det_db_score_mode='fast', det_east_score_thresh=0.8, det_east_cover_thresh=0.1, det_east_nms_thresh=0.2, det_sast_score_thresh=0.5, det_sast_nms_thresh=0.2, det_pse_thresh=0, det_pse_box_thresh=0.85, det_pse_min_area=16, det_pse_scale=1, scales=[8, 16, 32], alpha=1.0, beta=1.0, fourier_degree=5, rec_algorithm='SVTR_LCNet', rec_model_dir='/home/choigawoon/.paddleocr/whl/rec/korean/korean_PP-OCRv4_rec_infer', rec_image_inverse=True, rec_image_shape='3, 48, 320', rec_batch_num=6, max_text_length=25, rec_char_dict_path='/home/choigawoon/miniconda3/envs/carnumber_paddleocr/lib/python3.10/site-packages/paddleocr/ppocr/utils/dict/korean_dict.txt', use_space_char=True, vis_font_path='./doc/fonts/simfang.ttf', drop_score=0.5, e2e_algorithm='PGNet', e2e_model_dir=None, e2e_limit_side_len=768, e2e_limit_type='max', e2e_pgnet_score_thresh=0.5, e2e_char_dict_path='./ppocr/utils/ic15_dict.txt', e2e_pgnet_valid_set='totaltext', e2e_pgnet_mode='fast', use_angle_cls=False, cls_model_dir='/home/choigawoon/.paddleocr/whl/cls/ch_ppocr_mobile_v2.0_cls_infer', cls_image_shape='3, 48, 192', label_list=['0', '180'], cls_batch_num=6, cls_thresh=0.9, enable_mkldnn=False, cpu_threads=10, use_pdserving=False, warmup=False, sr_model_dir=None, sr_image_shape='3, 32, 128', sr_batch_num=1, draw_img_save_dir='./inference_results', save_crop_res=False, crop_res_save_dir='./output', use_mp=False, total_process_num=1, process_id=0, benchmark=False, save_log_path='./log_output/', show_log=True, use_onnx=False, output='./output', table_max_len=488, table_algorithm='TableAttn', table_model_dir=None, merge_no_span_structure=True, table_char_dict_path=None, layout_model_dir=None, layout_dict_path=None, layout_score_threshold=0.5, layout_nms_threshold=0.5, kie_algorithm='LayoutXLM', ser_model_dir=None, re_model_dir=None, use_visual_backbone=True, ser_dict_path='../train_data/XFUND/class_list_xfun.txt', ocr_order_method=None, mode='structure', image_orientation=False, layout=True, table=True, ocr=True, recovery=False, use_pdf2docx_api=False, invert=False, binarize=False, alphacolor=(255, 255, 255), lang='korean', det=True, rec=True, type='ocr', ocr_version='PP-OCRv4', structure_version='PP-StructureV2')
-[2024/03/24 16:46:17] ppocr INFO: **********temp.jpg**********
-[2024/03/24 16:46:18] ppocr DEBUG: dt_boxes num : 0, elapsed : 0.10128092765808105
-[2024/03/24 16:46:18] ppocr DEBUG: rec_res num  : 0, elapsed : 7.152557373046875e-07
--->
