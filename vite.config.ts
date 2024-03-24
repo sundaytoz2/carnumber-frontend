35551ef6260370bf484cb92b6974ac6bb3b05abf
choigawoon <choigawoon@gmail.com>
2024-03-24 16:37:02 +0900

    add proxy

--- vite.config.ts
+++ vite.config.ts
@@ -6,13 +6,24 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-  ],
+  // nginx의 location하고, router의 경로가 모두 일치해야함
+  // https://chat.openai.com/share/4fbfe114-d077-426a-9801-881df8465e83
+  // base: '/aivera/',
+  plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  server: {
+    proxy: {
+      // API요청을 프록시할 링크
+      '/api': {
+        target: 'http://127.0.0.1:8000',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
     }
   }
 })
